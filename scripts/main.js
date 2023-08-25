7f13a3ef4512a31cb2dcc596982f07cfdca3b2e7
JC144 <1168432+JC144@users.noreply.github.com>
2023-08-25 17:29:20 +0200

    * Amélioration de l'affichage * Suppression du détail

--- scripts/main.js
+++ scripts/main.js
@@ -43,74 +43,61 @@ function onFileImported(e) {
 
 function displayResults(kva, jourZenPlus) {
     viewManager.displayNextView();
+    const results = []
     abonnements.forEach((abo) => {
         let result = {};
         result.tarif = calculator.getTarif(kva, data, abo, jourZenPlus);
         result.title = abo.name;
-        displayTarif(result);
+        results.push(result);
     });
-}
 
-function getMonthName(monthNumber) {
-    const date = new Date();
-    date.setMonth(monthNumber - 1);
+    const yearsAvailableWithConso = results[0].tarif.map((v) => ({ year: v.year, conso: v.conso }));
+    yearsAvailableWithConso.forEach((currentYearAndConso) => {
+        const divYear = document.createElement("div");
+        divYear.className = "mb-4";
+        pricesResultRow.appendChild(divYear);
+        const titleYear = document.createElement("h3");
+        divYear.appendChild(titleYear);
+        const titleConso = document.createElement("h4");
+        divYear.appendChild(titleConso);
 
-    let monthString = date.toLocaleString('fr-FR', { month: 'long' });
-    return monthString.charAt(0).toUpperCase() + monthString.slice(1)
-}
+        titleYear.innerHTML = "Mars " + (currentYearAndConso.year - 1) + " à Mars " + currentYearAndConso.year;
+        titleConso.innerHTML = "Consommation : " + (currentYearAndConso.conso / 1000).toFixed(2) + "kWh";
 
-function displayTarif(result) {
-    const pricesResultRow = document.getElementById("pricesResultRow");
-    const pricesTitleRow = document.getElementById("pricesTitleRow");
-    const detailMonthyByMonth = document.getElementById("div-detailPrice");
-    const detailCol = document.createElement("div");
-    detailMonthyByMonth.appendChild(detailCol);
+        const table = document.createElement("table");
+        table.className = "table";
+        divYear.appendChild(table);
+        const tableHeader = document.createElement("thead");
+        table.appendChild(tableHeader);
+        const rowHeader = document.createElement("tr");
+        tableHeader.appendChild(rowHeader);
+        const headerTarifName = document.createElement("th");
+        headerTarifName.innerHTML = "Tarif";
+        rowHeader.appendChild(headerTarifName);
+        const headerEstimationName = document.createElement("th");
+        headerEstimationName.innerHTML = "Estimation";
+        rowHeader.appendChild(headerEstimationName);
 
-    const resultCol = document.createElement("div");
-    resultCol.classList.add("col-sm");
-    detailCol.classList.add("col-sm");
-    pricesResultRow.appendChild(resultCol);
+        const tableBody = document.createElement("tbody");
+        table.appendChild(tableBody);
 
-    const titleCol = document.createElement("div");
-    titleCol.classList.add("col-sm");
-    pricesTitleRow.appendChild(titleCol);
+        results.map((r) => ({
+            price: r.tarif
+                .find((t) => t.year == currentYearAndConso.year).price,
+            title: r.title
+        }))
+            .sort((a, b) => a.price - b.price)
+            .forEach(result => {
+                const tarifRow = document.createElement("tr");
+                tableBody.appendChild(tarifRow);
 
-    const tarifHeader = document.createElement("h3");
-    tarifHeader.classList.add("text-center");
-    tarifHeader.classList.add("h3");
-    tarifHeader.innerHTML = result.title;
-    titleCol.appendChild(tarifHeader);
+                const cellTarifName = document.createElement("th");
+                tarifRow.appendChild(cellTarifName);
+                cellTarifName.innerHTML = result.title;
 
-    const year = document.createElement("p");
-    year.classList.add("text-center");
-    year.classList.add("mt-4");
-    year.classList.add("mb-4");
-    resultCol.appendChild(year);
-
-    let displayConsommationAndPrice = "";
-    result.tarif.forEach((y) => {
-        displayConsommationAndPrice += "Mars " + (y.year - 1) + " à Mars " + y.year + "<br />";
-        displayConsommationAndPrice += "Consommation : " + (y.conso / 1000).toFixed(2) + "kWh <br/>";
-        displayConsommationAndPrice += "Estimation : " + y.price.toFixed(2) + " € TTC <br/><br/> ";
-    });
-    year.innerHTML = displayConsommationAndPrice;
-
-    const listOfMonths = document.createElement("ul");
-    detailCol.appendChild(listOfMonths);
-
-    result.tarif.forEach((y) => {
-        y.months.forEach((m) => {
-            const month = document.createElement("li");
-            listOfMonths.appendChild(month);
-            month.innerHTML = getMonthName(parseInt(m.month)) + "<br/>" + (m.conso / 1000).toFixed(2) + "kWh<br/>" + m.price.toFixed(2) + "€";
-
-            const listOfDays = document.createElement("ul");
-            month.appendChild(listOfDays);
-            for (let j = m.days.length - 1; j >= 0; j--) {
-                const day = document.createElement("li");
-                day.innerHTML = m.days[j].date + "<br/>" + (m.days[j].conso / 1000).toFixed(2) + "kWh<br/>HC: " + m.days[j].priceHC.toFixed(2) + "€ <br/>HP: " + m.days[j].priceHP.toFixed(2) + "€ <br/>" + m.days[j].price.toFixed(2) + "€";
-                listOfDays.appendChild(day);
-            }
-        });
+                const cellTarifPrice = document.createElement("td");
+                tarifRow.appendChild(cellTarifPrice);
+                cellTarifPrice.innerHTML = result.price.toFixed(2) + " € TTC";
+            });
     });
 }
