ef222b77249347fab7518b2074adb06b90ce12aa
J0hnMatrix <bocal83@gmail.com>
2024-02-10 08:45:49 +0100

    Update ElectriciteVerteVariable.js
    Mise à jour 10/02/2024

--- scripts/tarifs/ekwateur/ElectriciteVerteVariable.js
+++ scripts/tarifs/ekwateur/ElectriciteVerteVariable.js
@@ -6,63 +6,63 @@ abonnements.push(
             puissance: 3,
             abonnement: 13.29,
             bleu: {
-                prixKwhHC: 19.4
+                prixKwhHC: 29.64
             }
         },
         {
             puissance: 6,
             abonnement: 16.54,
             bleu: {
-                prixKwhHC: 20.96
+                prixKwhHC: 29.64
             }
         },
         {
             puissance: 9,
             abonnement: 19.95,
             bleu: {
-                prixKwhHC: 21.2
+                prixKwhHC: 33.0
             }
         },
         {
             puissance: 12,
             abonnement: 23.29,
             bleu: {
-                prixKwhHC: 21.8
+                prixKwhHC: 33.0
             }
         },
         {
             puissance: 15,
             abonnement: 26.43,
             bleu: {
-                prixKwhHC: 21.8
+                prixKwhHC: 33.0
             }
         },
         {
             puissance: 18,
             abonnement: 29.69,
             bleu: {
-                prixKwhHC: 21.8
+                prixKwhHC: 33.0
             }
         },
         {
             puissance: 24,
             abonnement: 36.31,
             bleu: {
-                prixKwhHC: 21.876
+                prixKwhHC: 33.0
             }
         },
         {
             puissance: 30,
             abonnement: 45.04,
             bleu: {
-                prixKwhHC: 21.8
+                prixKwhHC: 33.0
             }
         },
         {
             puissance: 36,
             abonnement: 52.8,
             bleu: {
-                prixKwhHC: 21.8
+                prixKwhHC: 33.0
             }
         }],
         hc: [{
