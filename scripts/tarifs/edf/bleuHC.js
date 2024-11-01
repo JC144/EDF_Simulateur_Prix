1efce07492c7b1a2840f285cd3767e6bc162a0bd
DuchkPy <duchkpy-gh@laposte.net>
2024-11-01 08:47:10 +0100

    Update bleuHC.js

--- scripts/tarifs/edf/bleuHC.js
+++ scripts/tarifs/edf/bleuHC.js
@@ -2,14 +2,14 @@ abonnements.push(
     {
         name: "EDF - Bleu Heures Creuses",
         offer_type: "TRV",
-        lastUpdate: "2024-02-01",
+        lastUpdate: "2024-11-01",
         isCommunity: false,
         subscription_url: "https://particulier.edf.fr/fr/accueil/electricite-gaz/offres-electricite/tarif-bleu.html",
         price_url: "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/Grille_prix_Tarif_Bleu.pdf",
         prices: [
             {
                 puissance: 6,
-                abonnement: 13.01,
+                abonnement: 13.09,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
@@ -25,7 +25,7 @@ abonnements.push(
             },
             {
                 puissance: 12,
-                abonnement: 20.13,
+                abonnement: 20.28,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
@@ -33,7 +33,7 @@ abonnements.push(
             },
             {
                 puissance: 15,
-                abonnement: 23.40,
+                abonnement: 23.57,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
@@ -41,7 +41,7 @@ abonnements.push(
             },
             {
                 puissance: 18,
-                abonnement: 26.64,
+                abonnement: 26.84,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
@@ -49,7 +49,7 @@ abonnements.push(
             },
             {
                 puissance: 24,
-                abonnement: 33.44,
+                abonnement: 33.70,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
@@ -57,7 +57,7 @@ abonnements.push(
             },
             {
                 puissance: 30,
-                abonnement: 39.63,
+                abonnement: 38.97,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
@@ -65,7 +65,7 @@ abonnements.push(
             },
             {
                 puissance: 36,
-                abonnement: 44.79,
+                abonnement: 45.08,
                 bleu: {
                     prixKwhHP: 27.00,
                     prixKwhHC: 20.68
