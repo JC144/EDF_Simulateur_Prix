13e0f58bdf661cc41cf36daf9e99bc5b67eb4c7f
Dawey <me@dawey.fr>
2024-11-05 22:40:04 +0100

    Update Vert Elec HC

--- scripts/tarifs/edf/vertHC.js
+++ scripts/tarifs/edf/vertHC.js
@@ -1,14 +1,14 @@
 abonnements.push({
     name: "EDF - Vert Electrique Heures Creuses",
     offer_type: "TRV",
-    lastUpdate: "2024-03-19",
+    lastUpdate: "2024-11-01",
     isCommunity: false,
     subscription_url: "https://particulier.edf.fr/fr/accueil/electricite-gaz/offres-electricite/offres-marche/electricite-verte/vert-electrique.html",
     price_url: "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/grille-prix-vert-electrique.pdf",
     prices: [
         {
             puissance: 6,
-            abonnement: 13.01,
+            abonnement: 13.09,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -16,7 +16,7 @@ abonnements.push({
         },
         {
             puissance: 9,
-            abonnement: 16.71,
+            abonnement: 16.82,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -24,7 +24,7 @@ abonnements.push({
         },
         {
             puissance: 12,
-            abonnement: 20.13,
+            abonnement: 20.28,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -32,7 +32,7 @@ abonnements.push({
         },
         {
             puissance: 15,
-            abonnement: 23.40,
+            abonnement: 23.09,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -40,7 +40,7 @@ abonnements.push({
         },
         {
             puissance: 18,
-            abonnement: 26.64,
+            abonnement: 26.26,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -48,7 +48,7 @@ abonnements.push({
         },
         {
             puissance: 24,
-            abonnement: 33.44,
+            abonnement: 32.92,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -56,7 +56,7 @@ abonnements.push({
         },
         {
             puissance: 30,
-            abonnement: 38.73,
+            abonnement: 38.97,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
@@ -64,7 +64,7 @@ abonnements.push({
         },
         {
             puissance: 36,
-            abonnement: 44.79,
+            abonnement: 45.08,
             bleu: {
                 prixKwhHP: 26.18,
                 prixKwhHC: 19.86
