e808ee55852b4d226781d416cba05dba01777c82
Dawey <me@dawey.fr>
2024-11-20 22:11:30 +0100

    Add Vert Elec WE Base/HC

---
+++ scripts/tarifs/edf/vertWeekEnd.js
@@ -0,0 +1,108 @@
+abonnements.push({
+    name: "EDF - Vert Electrique Week-End",
+    offer_type: "Marché",
+    lastUpdate: "2024-11-01",
+    isCommunity: false,
+    subscription_url: "https://particulier.edf.fr/fr/accueil/electricite-gaz/offres-electricite/offres-marche/electricite-verte/vert-electrique-week-end.html",
+    price_url: "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/grille-prix-vert-electrique-weekend.pdf",
+    prices: [
+        {
+            puissance: 6,
+            abonnement: 12.68,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 9,
+            abonnement: 15.89,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 12,
+            abonnement: 19.16,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 15,
+            abonnement: 22.21,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 18,
+            abonnement: 25.24,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 24,
+            abonnement: 31.96,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 30,
+            abonnement: 38.66,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        },
+        {
+            puissance: 36,
+            abonnement: 44.43,
+            bleu: {
+                prixKwhHC: 26.58
+            },
+            weekend: {
+                prixKwhHC: 19.37
+            }
+        }],
+    hc: [{
+        start: { hour: 0, minute: 0 },
+        end: { hour: 24, minute: 0 }
+    }],
+    hasHCCustom: false,
+    hasSpecialDaysCustom: false,
+    specialDays: [0, 6],
+    getDayType: function (day) {
+        let dayType = "bleu";
+
+        const isoDate = new Date(day.date);
+        const dayOfWeek = isoDate.getDay();
+
+        if (this.specialDays.includes(dayOfWeek)) {
+            dayType = "weekend";
+        }
+
+        return dayType;
+    }
+});
