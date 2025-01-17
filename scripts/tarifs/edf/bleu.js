d81cd3db88ab7c5bf0da754991954237e674ea28
Fabien <fabien@fmaurel.fr>
2025-01-17 18:55:48 +0100

    Update Tarifs bleu.js
    Tarifs kwh au 01/02/2025

--- scripts/tarifs/edf/bleu.js
+++ scripts/tarifs/edf/bleu.js
@@ -2,7 +2,7 @@ abonnements.push(
     {
         name: "EDF - Bleu",
         offer_type: "TRV",
-        lastUpdate: "2024-11-01",
+        lastUpdate: "2025-02-01",
         isCommunity: false,
         subscription_url: "https://particulier.edf.fr/fr/accueil/electricite-gaz/offres-electricite/tarif-bleu.html",
         price_url: "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/Grille_prix_Tarif_Bleu.pdf",
@@ -10,63 +10,63 @@ abonnements.push(
             puissance: 3,
             abonnement: 9.69,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 6,
             abonnement: 12.68,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 9,
             abonnement: 15.89,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 12,
             abonnement: 19.16,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 15,
             abonnement: 22.07,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 18,
             abonnement: 25.24,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 24,
             abonnement: 31.96,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 30,
             abonnement: 37.68,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         },
         {
             puissance: 36,
             abonnement: 44.43,
             bleu: {
-                prixKwhHC: 25.16
+                prixKwhHC: 20.16
             }
         }],
         hc: [{
