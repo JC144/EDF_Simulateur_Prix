9975ae99b2ad2456c53e6d33a7217de55f1f5d04
Dawey <dawey@pc.home>
2025-03-11 11:30:28 +0100

    Update Vert Electrique prices 2025-03-14

--- scripts/tarifs/edf/vert.js
+++ scripts/tarifs/edf/vert.js
@@ -2,24 +2,24 @@ abonnements.push(
     {
         name: "EDF - Vert Electrique",
         offer_type: "Marché",
-        lastUpdate: "2025-02-01",
+        lastUpdate: "2025-03-14",
         isCommunity: false,
         subscription_url: "https://particulier.edf.fr/fr/accueil/electricite-gaz/offres-electricite/offres-marche/electricite-verte/vert-electrique.html",
         price_url: "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/grille-prix-vert-electrique.pdf",
         prices: [
-            { puissance: 3, abonnement: 9.77 },
-            { puissance: 6, abonnement: 12.80 },
-            { puissance: 9, abonnement: 16.05 },
-            { puissance: 12, abonnement: 19.37 },
-            { puissance: 15, abonnement: 22.46 },
-            { puissance: 18, abonnement: 25.54 },
-            { puissance: 24, abonnement: 32.34 },
-            { puissance: 30, abonnement: 38.14 },
-            { puissance: 36, abonnement: 44.98 }
+            { puissance: 3, abonnement: 10.41 },
+            { puissance: 6, abonnement: 13.72 },
+            { puissance: 9, abonnement: 17.27 },
+            { puissance: 12, abonnement: 20.86 },
+            { puissance: 15, abonnement: 24.20 },
+            { puissance: 18, abonnement: 27.39 },
+            { puissance: 24, abonnement: 34.68 },
+            { puissance: 30, abonnement: 41.99 },
+            { puissance: 36, abonnement: 48.39 }
         ].map(item => ({
             ...item,
             bleu: {
-                prixKwhHC: 25.75
+                prixKwhHC: 19.36
             }
         })),
         hc: [{
@@ -39,26 +39,26 @@ abonnements.push(
 abonnements.push({
     name: "EDF - Vert Electrique Heures Creuses",
     offer_type: "Marché",
-    lastUpdate: "2025-02-01",
+    lastUpdate: "2025-03-14",
     isCommunity: false,
     subscription_url: "https://particulier.edf.fr/fr/accueil/electricite-gaz/offres-electricite/offres-marche/electricite-verte/vert-electrique.html",
     price_url: "https://particulier.edf.fr/content/dam/2-Actifs/Documents/Offres/grille-prix-vert-electrique.pdf",
     prices: [
-        { puissance: 6, abonnement: 13.21 },
-        { puissance: 9, abonnement: 17.01 },
-        { puissance: 12, abonnement: 20.51 },
-        { puissance: 15, abonnement: 23.34 },
-        { puissance: 18, abonnement: 26.55 },
-        { puissance: 24, abonnement: 33.30 },
-        { puissance: 30, abonnement: 39.43 },
-        { puissance: 36, abonnement: 45.62 }
+        { puissance: 6, abonnement: 14.04 },
+        { puissance: 9, abonnement: 18.01 },
+        { puissance: 12, abonnement: 21.69 },
+        { puissance: 15, abonnement: 24.64 },
+        { puissance: 18, abonnement: 28.11 },
+        { puissance: 24, abonnement: 35.40 },
+        { puissance: 30, abonnement: 41.96 },
+        { puissance: 36, abonnement: 48.72 }
     ].map(item => ({
         ...item,
         bleu: {
-            prixKwhHP: 27.71,
-            prixKwhHC: 21.38
+            prixKwhHP: 20.76,
+            prixKwhHC: 16.26
         }
-    })),    
+    })),
     hc: [],
     hasHCCustom: true,
     hasSpecialDaysCustom: false,
